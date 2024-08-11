aeaecfa1612dd13e585b932565bd3d9a08c176ae
abhi <geekyabhi6387@gmail.com>
2024-08-11 23:12:42 +0530

    Solutions till 07

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    splitString = string.split("")
+    reversedString = splitString.reverse().join("")
 
+    return reversedString;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fdeg) { 
+  return Math.round((fdeg - 32) * (5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cdeg) {
+ return Math.round(((cdeg * (9/5)) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
